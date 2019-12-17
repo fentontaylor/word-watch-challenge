b23cfe2f8ddd7b37312a16b355210658d5a5b57a
fentontaylor <fentontaylor@gmail.com>
2019-12-17 10:51:26 -0700

    Finally get ajax to work

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  $.ajax({
+    url: "https://wordwatch-api.herokuapp.com/api/v1/top_word",
+    context: document.body,
+    success: (res) => {
+      let top_word = Object.keys(res.word)[0]
+      let count = Object.values(res.word)[0]
+      $('.word-count').html(`${top_word}: ${count}`)
+    }
+  });
 })
