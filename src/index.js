3a001fcd6ad2d795af89c138e974ece71abc80cc
fentontaylor <fentontaylor@gmail.com>
2019-12-17 11:40:57 -0700

    Call displayTopWord function in post request

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  $.ajax({
-    url: "https://wordwatch-api.herokuapp.com/api/v1/top_word",
-    context: document.body,
-    success: (res) => {
-      let top_word = Object.keys(res.word)[0]
-      let count = Object.values(res.word)[0]
-      $('.word-count').html(`${top_word}: ${count}`)
-    }
-  });
+  function displayTopWord() {
+    $.ajax({
+      url: "https://wordwatch-api.herokuapp.com/api/v1/top_word",
+      context: document.body,
+      success: (res) => {
+        let top_word = Object.keys(res.word)[0]
+        let count = Object.values(res.word)[0]
+        $('.word-count').html(`${top_word}: ${count}`)
+      }
+    });
+  }
+  
+  displayTopWord();
 
   $("button").click(() => {
     let text = document.getElementsByTagName('textarea')[0].value
@@ -20,6 +24,7 @@ $(document).ready(() => {
         type: 'POST',
         data: { word: { value: w } }
       });
-    })
+    });
+    displayTopWord();
   });
 })
